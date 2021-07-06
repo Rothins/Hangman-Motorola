8065462babadc83c2a759d162b2b0ab8613b6044
Michał Majgier <86681325+Rothins@users.noreply.github.com>
2021-07-06 18:53:26 +0000

    Asking about type of guessing, created wrong used signs list and well used signs list, hearts, displaying the word.

--- main.cs
+++ main.cs
@@ -8,7 +8,7 @@ class theWord{
 
   public string country;
   public string lineOfDoc;
-
+  
   public theWord()
   {
     Random rnd = new Random();
@@ -36,7 +36,7 @@ class theWord{
        beginningOfTheCapital = i + 3;
        break;
      }
-
+     
    }
     for(int i = beginningOfTheCapital; i < lineOfDoc.Length; i++)
     {
@@ -49,10 +49,93 @@ class theWord{
 }
 
 
-class MainClass {
+class MainClass {  
  
  
   public static void Main () {
+    Console.WriteLine("Welcome to the Hangman game! \n");
+   theWord word = new theWord();
+  bool winningCondition = false;
+  short hearts = 5;
+  Console.WriteLine(word.capital);
+  for(int i = 0; i < word.capital.Length; i++)
+  {
+    if((word.capital.ToCharArray())[i] == ' ')
+    {
+      Console.Write("   ");
 
-}
-}
+    }else{
+      Console.Write("_ ");
+    }
+  }
+  List<char> wrongUsedSigns = new List<char>();
+  List<char> wellUsedSigns = new List<char>();
+
+  while(!winningCondition && hearts > 0)
+  {
+    Console.WriteLine("Do you want to guess the letter or the whole word? Press 'l' if a letter, and 'w' if the word");
+    char choice = Convert.ToChar(Console.ReadLine());
+    Console.WriteLine("\n");
+  if(choice == 'w')
+  {
+
+  }else{
+    char letter = Convert.ToChar(Console.ReadLine().ToUpper());
+    bool notUsed = true;
+    for(int i = 0; i < word.capital.Length; i++)
+    {
+      
+      if((word.capital.ToCharArray())[i] == letter)
+      {
+        wellUsedSigns.Add(letter);
+        notUsed = false;
+        break;
+      }
+    }
+    bool wrongUsed = false;
+    if(notUsed)
+    {
+      for(int i = 0; i < wrongUsedSigns.Count; i++)
+      {
+        if(wrongUsedSigns[i] == letter){
+          wrongUsed = true;
+          break;
+        }
+      }
+    if(!wrongUsed)
+    {
+      wrongUsedSigns.Add(letter);
+    }
+    }
+    for(int i = 0; i < word.capital.Length; i++)
+  {
+    if((word.capital.ToCharArray())[i] == ' ')
+    {
+      Console.Write("   ");
+
+    }else{
+      bool notGuessed = true;
+        for(int p = 0; p < wellUsedSigns.Count; p++)
+        {
+          if(wellUsedSigns[p] == (word.capital.ToCharArray())[i]){
+          Console.Write(wellUsedSigns[p]);
+          notGuessed = false;
+          break;
+          }
+       
+        }
+       if(notGuessed)
+              Console.Write("_ ");
+
+    }
+    }
+  }
+
+
+  }
+  if(winningCondition){
+    Console.WriteLine("You have won!");
+  }else{
+    Console.WriteLine("You have lost!");
+  }
+}}
